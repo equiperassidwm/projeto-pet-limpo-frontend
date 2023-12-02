0e4cc8a9d7410f8543f37e1742a288c040d00fc2
Equipe Rassi <equiperassidwm@gmaill.com>
2023-12-02 16:40:43 -0300

    Página Inicial finalizado v2 faltando footer

--- js/menu.js
+++ js/menu.js
@@ -1,23 +1,39 @@
-var menu = document.getElementById('menu');
+$('.menu').click(function () {
+    if ($('.itens-menu').hasClass('fechado')) {
+      abrirMenu();
+    } else {
+      fecharMenu();
+    }
+  });
 
-menu.addEventListener('click', abrirMenu)
-
-menu.addEventListener('click', trocarIcone)
+// menu.addEventListener('click', trocarIcone)
 
 function abrirMenu(){
-    let itensMenu = document.querySelector('.itens-menu');
-    $(itensMenu).slideToggle()
+    $('.itens-menu').addClass('aberto').animate({ right: '0' });
+    $('.itens-menu').removeClass('fechado')
+        // Anima a propriedade right para trazer o menu da direita para a esquerda
+        
 }
 
-function trocarIcone(){
-    let icone = document.getElementById('menu')
-
-    if(icone.classList.contains('fa-bars')){
-        icone.classList.remove('fa-bars')
-        icone.classList.add('fa-xmark')
-    }else{
-        icone.classList.remove('fa-xmark')
-        icone.classList.add('fa-bars')
-    }
-
+function fecharMenu() {
+    // Anima a propriedade right para mover o menu de volta para fora da tela
+    $('.itens-menu').removeClass('aberto').animate({ right: '-446px' })
+    $('.itens-menu').addClass('fechado')
 }
+
+
+
+
+
+// function trocarIcone(){
+//     let icone = document.querySelector('.menu-d')
+
+//     if(icone.classList.contains('fa-bars')){
+//         icone.classList.remove('fa-bars')
+//         icone.classList.add('fa-xmark')
+//     }else{
+//         icone.classList.remove('fa-xmark')
+//         icone.classList.add('fa-bars')
+//     }
+
+// }
